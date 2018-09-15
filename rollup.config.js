22fe06a6adf14d1facadad7eaf6ea4510000adec
Richard Nias <richardnias@gmail.com>
2018-09-15 21:03:56 +0100

    watch

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,23 @@
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
 import standard from 'rollup-plugin-standard'
+import { uglify } from 'rollup-plugin-uglify'
 
 export default {
-  entry: 'src/index.js',
-  dest: 'build/main.min.js',
-  format: 'iife',
-  sourceMap: 'inline',
+  input: 'src/index.js',
+  output: {
+    file: 'build/main.min.js',
+    format: 'iife',
+    sourceMap: 'inline'
+  },
   plugins: [
+    resolve({
+      module: true
+    }),
     standard(),
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    uglify()
   ]
 }
