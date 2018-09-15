9474a35c284764dc828da6775581a443c4551e00
Richard Nias <richardnias@gmail.com>
2018-09-15 20:31:29 +0100

    add linting

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import babel from 'rollup-plugin-babel'
+import standard from 'rollup-plugin-standard'
 
 export default {
-		  entry: 'src/index.js',
-		  dest: 'build/main.min.js',
-		  format: 'iife',
-		  sourceMap: 'inline',
-		plugins: [
-				babel({
-						exclude: 'node_modules/**',
-				})
-		]
+  entry: 'src/index.js',
+  dest: 'build/main.min.js',
+  format: 'iife',
+  sourceMap: 'inline',
+  plugins: [
+    standard(),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
 }
