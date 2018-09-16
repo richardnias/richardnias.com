9a2c4e87dbf4a0ad1c7db107dd81b5985853f3a4
Richard Nias <richardnias@gmail.com>
2018-09-16 21:52:19 +0100

    use systemjs

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import standard from 'rollup-plugin-standard'
-import {uglify} from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 
 export default {
-  input: ['src/mountains.js', 'src/oblong.js'],
+  input: ['src/index.js', 'src/mountains.js', 'src/oblong.js'],
   output: {
     dir: 'public/js',
-    format: 'esm',
+    format: 'system',
     sourcemap: true
   },
   experimentalCodeSplitting: true,
