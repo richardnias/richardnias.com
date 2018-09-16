a926f72fe81d00eefd77e5f7ae97fd279d183f90
Richard Nias <richardnias@gmail.com>
2018-09-16 11:57:34 +0100

    fix uglify

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,10 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import standard from 'rollup-plugin-standard'
+import {uglify} from 'rollup-plugin-uglify'
 
 export default {
-  input: ['src/index.js', 'src/oblong.js'],
+  input: ['src/mountains.js', 'src/oblong.js'],
   output: {
     dir: 'public/js',
     format: 'esm',
