7061552fbb8ee677575d50fde3932d82796ba924
Richard Nias <richardnias@gmail.com>
2019-05-28 00:13:30 +0200

    gzip in prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,16 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: common.plugins + [
+    new CompressionPlugin({
+      filename: '[path][query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      deleteOriginalAssets: false
+    }),
+  ]
 })
