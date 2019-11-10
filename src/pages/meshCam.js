99769c9ae42c0ead4be5e8b1bc734ca47ef1c88e
Richard Nias <richardnias@gmail.com>
2019-11-10 23:12:24 +0100

    initial implementation..

--- src/pages/meshCam.js
+++ src/pages/meshCam.js
@@ -2,7 +2,7 @@ import { Color } from 'three/src/math/Color'
 import { Mesh } from 'three/src/objects/Mesh'
 import { MeshBasicMaterial } from 'three/src/materials/MeshBasicMaterial'
 import { PerspectiveCamera } from 'three/src/cameras/PerspectiveCamera'
-import { PlaneBufferGeometry } from 'three/src/geometries/PlaneGeometry'
+import { PlaneGeometry } from 'three/src/geometries/PlaneGeometry'
 import { Scene } from 'three/src/scenes/Scene'
 import { VideoTexture } from 'three/src/textures/VideoTexture'
 import { LinearFilter, DoubleSide } from 'three/src/constants'
@@ -11,26 +11,43 @@ import { WebGLRenderer } from 'three/src/renderers/WebGLRenderer'
 import WebcamPage from '../lib/webcam'
 import Detector from '../lib/detector'
 
+const RESOLUTION = 20
+const GEOMETRY_WIDTH = RESOLUTION * 16
+const GEOMETRY_HEIGHT = RESOLUTION * 9
+const EXTRUSION_FACTOR = 10
+
+function getBrightness (r, g, b) {
+  return Math.max(r, g, b) / 256
+}
+
+function getPixelsAtCoords (x, y, imageWidth, imageData) {
+  const index = (x + y * imageWidth) * 4
+  return imageData.slice(index, index + 3)
+}
+
+function getBrightnessAtCoords (x, y, imageWidth, imageData) {
+  return getBrightness(...getPixelsAtCoords(x, y, imageWidth, imageData))
+}
+
 export default class MeshCamPage extends WebcamPage {
   constructor () {
     super()
-    this.errorMessage = 'WebGL is not supported in this browser'
+    this.errorMessage = 'WebGL is not supported in this browser or MediaDevices interface not available.'
   }
 
-  init () {
-    super.init()
+  async init () {
+    // todo: different aspect ratios
+    await super.init()
 
     const texture = new VideoTexture(this.video)
     texture.minFilter = LinearFilter
 
-    console.log(texture)
-
-    const geometry = new PlaneBufferGeometry(16, 9)
-    geometry.scale(0.5, 0.5, 0.5)
+    this.geometry = new PlaneGeometry(16, 9, GEOMETRY_WIDTH - 1, GEOMETRY_HEIGHT - 1)
+    this.geometry.scale(0.8, 0.8, 0.8)
 
     const material = new MeshBasicMaterial({ map: texture, side: DoubleSide })
 
-    const mesh = new Mesh(geometry, material)
+    const mesh = new Mesh(this.geometry, material)
     mesh.scale.x = -1
     mesh.position.set(0, 0, -10)
 
@@ -49,6 +66,18 @@ export default class MeshCamPage extends WebcamPage {
   animate () {
     super.animate()
 
+    const videoData = this.extractVideoData(0, 0, this.videoWidth, this.videoHeight).data
+
+    for (let i = 0; i < GEOMETRY_WIDTH; i++) {
+      for (let j = GEOMETRY_HEIGHT - 1; j >= 0; j--) {
+        const x = Math.floor(this.videoWidth * (1 - i / GEOMETRY_WIDTH))
+        const y = Math.floor(j / GEOMETRY_HEIGHT * this.videoHeight)
+        const vertexIndex = i + j * GEOMETRY_WIDTH
+        this.geometry.vertices[vertexIndex].z = EXTRUSION_FACTOR * (-1 + getBrightnessAtCoords(x, y, this.videoWidth, videoData))
+      }
+    }
+    this.geometry.verticesNeedUpdate = true
+
     this.renderer.render(this.scene, this.camera)
   }
 
@@ -60,6 +89,6 @@ export default class MeshCamPage extends WebcamPage {
   }
 
   isSupported () {
-    return Detector.webgl
+    return Detector.webgl && super.isSupported()
   }
 }
