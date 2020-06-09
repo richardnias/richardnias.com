b65445e79dd9a9d9d937661c6c250b4426963b77
Richard Nias <richardnias@gmail.com>
2020-06-09 21:48:03 +0200

    squares

---
+++ src/pages/squares.js
@@ -0,0 +1,70 @@
+import CanvasPage from "../lib/canvasPage";
+
+const SPEED = 0.001;
+const N = 20;
+
+export default class SinePage extends CanvasPage {
+  constructor() {
+    super();
+    this.inspiration = {
+      title: '"Making relaxing math gifs..."',
+      source: "IntertialObservr",
+      url: "https://twitter.com/InertialObservr/status/1240008000078614528",
+    };
+  }
+
+  drawSquare(p0, p1, p2, p3) {
+    let ctx = this.ctx;
+
+    ctx.beginPath();
+    ctx.moveTo(...p0);
+    ctx.lineTo(...p1);
+    ctx.lineTo(...p2);
+    ctx.lineTo(...p3);
+    ctx.lineTo(...p0);
+    ctx.stroke();
+  }
+
+  lerp([x0, y0], [x1, y1], t) {
+    let x = x0 + (x1 - x0) * t;
+    let y = y0 + (y1 - y0) * t;
+    return [x, y];
+  }
+
+  generateSquare([p0, p1, p2, p3], t) {
+    return [
+      this.lerp(p0, p1, t),
+      this.lerp(p1, p2, t),
+      this.lerp(p2, p3, t),
+      this.lerp(p3, p0, t),
+    ];
+  }
+
+  animate() {
+    super.animate();
+
+    this.t = (this.t + SPEED) % 1;
+
+    let square = this.firstSquare;
+    this.drawSquare(...square);
+    for (let i = 0; i < N; i++) {
+      square = this.generateSquare(square, this.t);
+      this.drawSquare(...square);
+    }
+  }
+
+  onResize() {
+    super.onResize();
+    this.halfWidth = window.innerWidth / 2;
+    this.halfHeight = window.innerHeight / 2;
+    this.halfSize = Math.min(this.halfWidth, this.halfHeight);
+    this.side = Math.sqrt(2 * this.halfSize ** 2);
+    this.firstSquare = [
+      [this.halfWidth, this.halfHeight - this.halfSize],
+      [this.halfWidth + this.halfSize, this.halfHeight],
+      [this.halfWidth, this.halfHeight + this.halfSize],
+      [this.halfWidth - this.halfSize, this.halfHeight],
+    ];
+    this.t = 0;
+  }
+}
--- src/pages/sunflower2.js
+++ src/pages/sunflower2.js
@@ -1,66 +1,67 @@
-import CanvasPage from '../lib/canvasPage'
+import CanvasPage from "../lib/canvasPage";
 
-const OFFSET = 3
-const PHI = (Math.sqrt(5) - 1) * Math.PI
-const DENSITY = 1.2
+const OFFSET = 3;
+const PHI = (Math.sqrt(5) - 1) * Math.PI;
+const DENSITY = 1.2;
 
 export default class SunflowerPage extends CanvasPage {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.inspiration = {
-      title: 'This spiral is rotated by the golden angle...',
-      source: 'matthen.com',
-      url: 'https://blog.matthen.com/post/127400828111/this-spiral-is-rotated-by-the-golden-angle-every',
-    }
+      title: '"This spiral is rotated by the golden angle..."',
+      source: "matthen.com",
+      url:
+        "https://blog.matthen.com/post/127400828111/this-spiral-is-rotated-by-the-golden-angle-every",
+    };
   }
 
-  getPolarCoords (index, rotationFactor) {
+  getPolarCoords(index, rotationFactor) {
     return {
       radius: OFFSET + index / DENSITY,
       theta: PHI * index + rotationFactor,
-    }
+    };
   }
 
-  polarCoordsToCartesian ({ radius, theta }) {
+  polarCoordsToCartesian({ radius, theta }) {
     return {
       x: this.canvas.width / 2 + Math.cos(theta) * radius,
       y: this.canvas.height / 2 + Math.sin(theta) * radius,
-      radius: Math.sqrt(radius/7),
-      alpha: Math.pow(radius / this.sunflowerRadius - 1, 2) * 2
-    }
+      radius: Math.sqrt(radius / 7),
+      alpha: Math.pow(radius / this.sunflowerRadius - 1, 2) * 2,
+    };
   }
 
-  drawPoint (ctx, { x, y, radius, alpha }) {
-    ctx.fillStyle = `rgba(255, 255, 255, ${alpha})`
-    ctx.beginPath()
-    ctx.arc(x, y, radius, 0, 2 * Math.PI)
-    ctx.fill()
+  drawPoint(ctx, { x, y, radius, alpha }) {
+    ctx.fillStyle = `rgba(255, 255, 255, ${alpha})`;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fill();
   }
 
-  animate () {
-    super.animate()
+  animate() {
+    super.animate();
 
     for (let i = 0; i < this.numPoints; i++) {
-      let polarCoords = this.getPolarCoords(i, this.rotationFactor)
-      let point = this.polarCoordsToCartesian(polarCoords)
-      this.drawPoint(this.ctx, point)
+      let polarCoords = this.getPolarCoords(i, this.rotationFactor);
+      let point = this.polarCoordsToCartesian(polarCoords);
+      this.drawPoint(this.ctx, point);
     }
 
     if (this.frameIndex % 2 === 0) {
-      this.rotationFactor += PHI
+      this.rotationFactor += PHI;
     }
 
-    this.frameIndex++
+    this.frameIndex++;
   }
 
-  onResize () {
-    super.onResize()
+  onResize() {
+    super.onResize();
     this.sunflowerRadius = Math.min(
-        window.innerWidth / 2 - OFFSET,
-        window.innerHeight / 2 - OFFSET
-      )
-    this.numPoints = this.sunflowerRadius * DENSITY
-    this.rotationFactor = 0
-    this.frameIndex = 0
+      window.innerWidth / 2 - OFFSET,
+      window.innerHeight / 2 - OFFSET
+    );
+    this.numPoints = this.sunflowerRadius * DENSITY;
+    this.rotationFactor = 0;
+    this.frameIndex = 0;
   }
 }
